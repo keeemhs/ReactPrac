7cdbedc5311a40f467465da18c812409aad13fb0
keeemhs <alenen0227@naver.com>
2023-10-10 20:56:49 +0900

    231010 commit

--- 41_react_typescript/src/Types.tsx
+++ 41_react_typescript/src/Types.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 interface Props {
     name?: string;
@@ -9,10 +9,14 @@ interface Data {
 }
 // 컴포넌트 제작 방법 2
 const Types: React.FC<Props> = (props) => {
-    const [count, setCount] = useState<Data>();
+    const [count, setCount] = useState<Data | null>();
+
+    const myInput = useRef<HTMLInputElement>(null);
+
     return (
         <>
             <h2>Hello {props.name}</h2>
+            <input ref={myInput} />
         </>
     );
 };
